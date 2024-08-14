5ab3a5534a19a6a08e7d71d3cf0a66be02d6615b
gianxz9 <161588605+gianxz9@users.noreply.github.com>
2024-08-14 08:08:52 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
-let vetor=[];
+let vetor = [];
 
 function adicionarValor() {
 
   const valor = Number(document.getElementById('valor').value);
 
+
   if (!isNaN(valor)) {
 
     vetor.push(valor);
 
-    const tabela = document.getElementById('tabelaValores').getElementByTagName('tbody')[0];
+    const tabela = document.getElementById("tabelaValores").getElementsByTagName('tbody')[0];
+
     const novaLinha = tabela.insertRow();
     const celula = novaLinha.insertCell(0);
     celula.textContent = valor;
@@ -18,3 +20,20 @@ function adicionarValor() {
   }
 
 }
+
+/*
+
+  function monstrarPares() {
+
+    const tabela = document.getElementById('tabelaPares').getElementByTagName('tbody')[0];
+
+    for (let i = 0; i < vetor.length; i++) {
+      const valor = vetor [i];
+      if(valor % 2 === 0) {
+        const linh = tabela .insertRow();
+        const celula = linha.insertCell();
+        celula.textContent = valor;
+      }
+    }
+  }
+*/
