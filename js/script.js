0464a09fd8222f671f592cee60b758a4d0ae6030
gianxz9 <161588605+gianxz9@users.noreply.github.com>
2024-08-20 08:47:52 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -21,6 +21,29 @@ function adicionarValor() {
 
 }
 
+ function contarNumeros() {
+
+   const tabela = document.getElementById('tabelaNumeros').getElementsByTagName('tbody')[0];
+
+        let positivos = 0;
+        let negativos = 0;
+
+        vetor.forEach(num => {
+          const linha = tabela.insertRow();
+          const celulaNumero = linha.insertCell();
+          const celulaTipo = linha.insertCell();
+            if (num > 0) {
+              positivos++;
+              celulaTipo.textContent = 'Positivo';
+            } else {
+              negativos++;
+              celulaTipo.textContent = 'Negativo';
+            }
+        });
+
+
+ }
+
 /*
 
   function monstrarPares() {
